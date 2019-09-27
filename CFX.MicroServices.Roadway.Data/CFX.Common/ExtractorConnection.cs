385b6e161f78105df7396a888af0572dc9e4c3aa
jramirez3022 <52212696+jramirez3022@users.noreply.github.com>
2019-09-27 16:23:59 -0400

    Saving Changes

---
+++ CFX.MicroServices.Roadway.Data/CFX.Common/ExtractorConnection.cs
@@ -0,0 +1,15 @@
+﻿
+using gov.its.c2cClient; 
+
+namespace CFX.Common
+{
+    public class ExtractorConnection
+    {
+        ExtractorConn extractorConn;
+
+        public void ConnectToExtractor()
+        {
+
+        }
+    }
+}
